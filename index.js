41e4f90f6add719663c56a0963b5f5aa9882d5bc
sshishi <khashi11@yahoo.com>
2017-10-02 17:10:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var name = "Joe"
+var name = "susan"
 var height = "74"
+if(name==="susan"){
+  console.log("the name is correct")
+} else 
+  console.log("Expected the name to be susan")
+
 
 
 // Don't worry about this
